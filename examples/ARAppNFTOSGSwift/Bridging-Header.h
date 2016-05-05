44810cc5a1db9873b5738d045a28c828e41ecb58
Ruslan Musagitov <ruslan.musagitov@gmail.com>
2016-05-05 19:10:23 +0600

    bridging header populated, app camera test removed

--- examples/ARAppNFTOSGSwift/Bridging-Header.h
+++ examples/ARAppNFTOSGSwift/Bridging-Header.h
@@ -9,18 +9,22 @@
 #ifndef Bridging_Header_h
 #define Bridging_Header_h
 
-//#import <AR/ar.h>
-//#import <AR/video.h>
-//#import <AR/gsub_es.h>
-//#import "ARView.h"
+#import <AR/ar.h>
+#import <AR/video.h>
+#import <AR/gsub_es.h>
+#import "../ARAppCore/ARView.h"
 //#import "../ARAppCore/ARMarker.h"
 //#import "../ARAppCore/VirtualEnvironment.h"
-//#import <AR/sys/CameraVideo.h>
+#import <AR/sys/CameraVideo.h>
 //
-//#import <pthread.h>
-//#import <thread_sub.h>
-//#import <AR2/tracking.h>
-//#import <KPM/kpm.h>
+#import <pthread.h>
+#import <thread_sub.h>
+#import <AR2/tracking.h>
+#import <KPM/kpm.h>
+#import "ARViewController.h"
+#import <AR/gsub_es.h>
+#import "../ARAppCore/ARMarkerNFT.h"
+#import "../ARAppCore/trackingSub.h"
 
 
 #endif /* Bridging_Header_h */
