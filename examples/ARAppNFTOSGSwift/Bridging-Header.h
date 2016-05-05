e8393b366cd7ee1288324fd2ea86301736417f81
Ruslan Musagitov <ruslan.musagitov@gmail.com>
2016-05-05 18:54:05 +0600

    initial commit

---
+++ examples/ARAppNFTOSGSwift/Bridging-Header.h
@@ -0,0 +1,26 @@
+//
+//  Bridging-Header.h
+//  ARToolKit5iOS
+//
+//  Created by Ruslan Musagitov on 05/05/16.
+//
+//
+
+#ifndef Bridging_Header_h
+#define Bridging_Header_h
+
+//#import <AR/ar.h>
+//#import <AR/video.h>
+//#import <AR/gsub_es.h>
+//#import "ARView.h"
+//#import "../ARAppCore/ARMarker.h"
+//#import "../ARAppCore/VirtualEnvironment.h"
+//#import <AR/sys/CameraVideo.h>
+//
+//#import <pthread.h>
+//#import <thread_sub.h>
+//#import <AR2/tracking.h>
+//#import <KPM/kpm.h>
+
+
+#endif /* Bridging_Header_h */
